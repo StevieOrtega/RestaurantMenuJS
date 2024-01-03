2f78254caac112af5eaa1dac044053911365072b
StevieOrtega <stevenvortega@gmail.com>
2024-01-03 14:53:23 -0800

    update 3.0

--- app.js
+++ app.js
@@ -1,134 +1,126 @@
-/* 
--------Booleans are Data-------
+ 
+// -------Booleans are Data-------
 
-getClearance = allowed => (allowed ? 'access allowed' : 'access denied')
-result1 = getClearance(true)
-result2 = getClearance(false)
+// getClearance = allowed => (allowed ? 'access allowed' : 'access denied')
+// result1 = getClearance(true)
+// result2 = getClearance(false)
 
-console.log(result1) // access allowed; 
-console.log(result2) // access denied; 
+// console.log(result1) // access allowed; 
+// console.log(result2) // access denied; 
 
+// -------objects are data -------
+// getFirstName = (obj) => obj.firstName 
 
+// result = getFirstName({
+//     firstName: 'yazeed'
+// })
 
--------objects are data -------
-getFirstName = (obj) => obj.firstName 
+// console.log(result)
 
-result = getFirstName({
-    firstName: 'yazeed'
-})
+// -------Arrays are data-------
 
-console.log(result)
+// len = (array) => array.length
 
+// result = len([1, 2, 3])
 
--------Arrays are data-------
+// console.log(result) // 3 
 
-len = (array) => array.length
-
-result = len([1, 2, 3])
-
-console.log(result) // 3 
-
-
-
-----------------------------
-Strings, Numbers, objects, booleans, and arrays are all data
+// ----------------------------
+// Strings, Numbers, objects, booleans, and arrays are all data
 
 
-these 5 types are first class citizien in every mainstream 
-language 
+// these 5 types are first class citizien in every mainstream 
+// language 
 
-what makes them first class? you can pass them around. store them
-in varaibles and arrays, use them as inputs for calculations 
-you can use them like any piece of data 
+// what makes them first class? you can pass them around. store them
+// in varaibles and arrays, use them as inputs for calculations 
+// you can use them like any piece of data 
 
-* pass them 
-*store them 
-    - variables 
-    - arrays
-    - inputs 
+// * pass them 
+// *store them 
+//     - variables 
+//     - arrays
+//     - inputs 
+  
+// functions can be data too 
+// 4ways fucntions are data in javascript
 
+// 1. pass them to other functions 
+//     a fucntion that takes or retuns another function has become higher-order 
+//     higher order functions lets us abstract common actions like map, filter and reduce 
 
-    
-functions can be data too 
-4ways fucntions are data in javascript
-
-1. pass them to other functions 
-    a fucntion that takes or retuns another function has become higher-order 
-    higher order functions lets us abstract common actions like map, filter and reduce 
-
-
-2. set them as object properties 
-    that makes them methods
+// 2. set them as object properties 
+//     that makes them methods
 
 
-3. store them in arrays
-    useful if you're calling a list of functions in response to an event. the 
-    javascript event looop works like this
+// 3. store them in arrays
+//     useful if you're calling a list of functions in response to an event. the 
+//     javascript event looop works like this
 
 
-4. set them as variables
-    refercing a fuction make it easy to reuse, espiecally curried funcitons 
-    that take some pararms now and others later!
+// 4. set them as variables
+//     refercing a fuction make it easy to reuse, espiecally curried funcitons 
+//     that take some pararms now and others later!
 
-functions as agruments 
+// functions as agruments 
 
-isEven = (num) => num % 2 === 0; 
-result = [1, 2, 3, 4].filter(isEven)
+// isEven = (num) => num % 2 === 0; 
+// result = [1, 2, 3, 4].filter(isEven)
 
-console.log(result)
+// console.log(result)
 
-see how filter uses isEven to decide what numbers to keep? isEven, a 
-function, was a parameter to another function.
+// see how filter uses isEven to decide what numbers to keep? isEven, a 
+// function, was a parameter to another function.
 
-its called by filter for each number, and uses the returned value true or false
-to determine if a number should be kept or discarded 
-
-
--------returning functions-------
-
-add = (x) => (y) => x + y
-
-add requires two parameters, but not all at once. it's a function asking 
-for just x, that returns a function asking for just y.
-
-Again, this is only possible because JS allows functions to be a return value
-just like strings, numbers, booleans, etc. 
-
-You can still supply x and y immediately, if you wish with a double invocation 
-
-result = add(10)(20)
-console.log(result) // 30
+// its called by filter for each number, and uses the returned value true or false
+// to determine if a number should be kept or discarded 
 
 
-or x now and Y later 
+// -------returning functions-------
 
-add10 = add(10)
-result = add10(20)
+// add = (x) => (y) => x + y
 
-console.log(result) // 30
+// add requires two parameters, but not all at once. it's a function asking 
+// for just x, that returns a function asking for just y.
 
--------Greater Resuablilty-------
+// Again, this is only possible because JS allows functions to be a return value
+// just like strings, numbers, booleans, etc. 
 
-Probaly the greatest benefit of HOFs is greater reusability. Without them 
-Js's premiere array methods - map, filter, and reduce wouldnt exist 
+// You can still supply x and y immediately, if you wish with a double invocation 
 
-here a list of users. were going to do some calculations with their information
+// result = add(10)(20)
+// console.log(result) // 30
 
 
-Map 
-without higher-order functions, we'd always need loops to mimic maps
-functionality
+// or x now and Y later 
 
-getName = (user) => user.name
-userName = []
+// add10 = add(10)
+// result = add10(20)
 
-for (let i = 0; i < users.length; i++){
-    const name = getName(user[i])
+// console.log(result) // 30
 
-    userNames.push(name)
-}
+// -------Greater Resuablilty-------
 
-console.log(userNames)
+// Probaly the greatest benefit of HOFs is greater reusability. Without them 
+// Js's premiere array methods - map, filter, and reduce wouldnt exist 
+
+// here a list of users. were going to do some calculations with their information
+
+
+// Map 
+// without higher-order functions, we'd always need loops to mimic maps
+// functionality
+
+// getName = (user) => user.name
+// userName = []
+
+// for (let i = 0; i < users.length; i++){
+//     const name = getName(user[i])
+
+//     userNames.push(name)
+// }
+
+// console.log(userNames)
 
 // we made a function and it returns the name of the user 
 // userName is an empty array 
@@ -137,16 +129,16 @@ console.log(userNames)
 // until we reach the end of the users length and. in getName we pass the user array
 // and then we push those names into the userName array while we loop
 
-annd then we console.log the array
+// annd then we console.log the array
 
 
-we could do this instead of all that 
+// we could do this instead of all that 
 
 
-let getName = (user) => user.name
-userNames = users.map(getName)
+// let getName = (user) => user.name
+// userNames = users.map(getName)
 
-console.log(userNames)
+// console.log(userNames)
 // we still make the function as we are suppose to but.... 
 // now we use the map function instead so we dont have use a for loop 
 // the map function will use the call the getname function and use that to get the name 
@@ -154,19 +146,17 @@ console.log(userNames)
 // then we  console.log the variable
 
 
-summary
+// summary
 
- * strings, numbers, bools, arrays, and objects can be stored as variables
-, arrays, and properties or methods
+//  * strings, numbers, bools, arrays, and objects can be stored as variables
+// , arrays, and properties or methods
 
-    * javascript treats functions the same way
-    * this allows for functions that operate on other functions: higer-order
-    * functions
-    * Map, filter, and reduce are prime examples and make common patterns like transforming, searching, 
-    * and summing liss much easier
-    * string, numbers, bool, and array and objects can be strored as bar
-
-*/ 
+//     * javascript treats functions the same way
+//     * this allows for functions that operate on other functions: higer-order
+//     * functions
+//     * Map, filter, and reduce are prime examples and make common patterns like transforming, searching, 
+//     * and summing liss much easier
+//     * string, numbers, bool, and array and objects can be strored as bar
 
 // users = [
 //     {
@@ -205,6 +195,102 @@ summary
 // let totalAge = users.reduce((total, user) => user.age + total, 0)
 
 // console.log(totalAge)
+
+
+breakfastItems = [
+    {
+        name: "Buttermilk Pancakes Platter",
+        price: 10.99,
+        text: "Pancakes made soft and fluffy by scratching and with love to give it that home feel taste."
+    },
+    {
+        name: "Bacon & Scambled Eggs Platter",
+        price: 10.99,
+        text: "Pancakes made soft and fluffy by scratching and with love to give it that home feel taste."
+    },
+    
+    {
+        name: "Sausage Patty Platter",
+        price: 10.99,
+        text: "Pancakes made soft and fluffy by scratching and with love to give it that home feel taste."
+    },
+    {
+        name: "Omelete Platter",
+        price: 10.99,
+        text: "Pancakes made soft and fluffy by scratching and with love to give it that home feel taste."
+    },
+
+    
+]
+
+lunchItems = [
+    {
+        name: "Turkey & Avocado Sandwich",
+        price: 10.99,
+        text: "Pancakes made soft and fluffy by scratching and with love to give it that home feel taste."
+    },
+    {
+        name: "Tuna Melt Sandwich",
+        price: 10.99,
+        text: "Pancakes made soft and fluffy by scratching and with love to give it that home feel taste."
+    },
+    {
+        name: "Carne Asada Nachos",
+        price: 10.99,
+        text: "Pancakes made soft and fluffy by scratching and with love to give it that home feel taste."
+    },
+    {
+        name: "Roadhouse Burger",
+        price: 10.99,
+        text: "Pancakes made soft and fluffy by scratching and with love to give it that home feel taste."
+    },
+]
+
+shakes = [
+    {
+        name: "Oreo shake",
+        price: 7.99,
+        text: "Blended Oreo cookies with a vanilla based milkshake"
+    },
+    {
+        name: "Vanilla shake",
+        price: 7.99,
+        text: "Blended Oreo cookies with a vanilla based milkshake"
+    },
+    {
+        name: "Chocolate shake",
+        price: 7.99,
+        text: "Blended Oreo cookies with a vanilla based milkshake"
+    },
+    {
+        name: "Strawberry shake",
+        price: 7.99,
+        text: "Blended Oreo cookies with a vanilla based milkshake"
+    },
+]
+
+dinnerItems = [
+    {
+        name: "T-bone Steak",
+        price: 10.99,
+        text: "Pancakes made soft and fluffy by scratching and with love to give it that home feel taste."
+    },
+    {
+        name: "Fish & Chips",
+        price: 10.99,
+        text: "Pancakes made soft and fluffy by scratching and with love to give it that home feel taste."
+    },
+    {
+        name: "Fried Chicken Sandwich",
+        price: 10.99,
+        text: "Pancakes made soft and fluffy by scratching and with love to give it that home feel taste."
+    },
+    {   
+        name: "Fettucine",
+        price: 10.99,
+        text: "Pancakes made soft and fluffy by scratching and with love to give it that home feel taste."
+    },
+]
 
 
 
--- readMe.txt
+++ readMe.txt
@@ -1,9 +1,43 @@
 When making this restraunt page, i need to cover these concepts:
-
+need to use higher function fucntions 
+function getUser(user) {
+    user.name;
+}
 1. arrays
 2. objects
+Breakfast 
+    * Picture of the plate
+    * Name of the plate
+    * price of the plate
+
+Lunch 
+    * picture of the plate 
+    * name of the plate 
+    * price of the plate
+    * random text about the plate
+
+Dinner
+    * picture of the plate
+    * name of the plate 
+    * price of the plate 
+    * Summary of the plate
+
 3.forEach()
+    For each of these elements in the array we are going to place
+    them in their own div 
 4. DOMContentLoaded
+    more research 
+
 5. map, filter, and reduce
+        use map and filter and reduce to properly implement them into their proper
+        locations in the with the tag breakfast o lunch o shakes o dinner 
+    
 6. innerHTML
+    we can append a new list  item to the existing list 
+    never use innerHTML for input. 
+
+
 7. includes method
+    the includes method of arrray instances determines whether an array 
+    includes a certain value among its entries, returning true or false
+    as approtipate 
